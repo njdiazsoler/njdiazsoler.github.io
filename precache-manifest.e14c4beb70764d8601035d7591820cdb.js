f7609b187d7aae8a83bbc7f953ee99ce2c889b9f
njdiazsoler <njdiaz90@gmail.com>
2019-03-11 15:53:43 -0300

    Updates

---
+++ precache-manifest.e14c4beb70764d8601035d7591820cdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "511b91da99e0ffdb78c1",
+    "url": "/static/js/main.7accaff0.chunk.js"
+  },
+  {
+    "revision": "31edd4d42714657cb2ba",
+    "url": "/static/js/2.9d8d7063.chunk.js"
+  },
+  {
+    "revision": "511b91da99e0ffdb78c1",
+    "url": "/static/css/main.7295f8c0.chunk.css"
+  },
+  {
+    "revision": "7ecf3332ed01d7c7cbf6552765920a88",
+    "url": "/index.html"
+  }
+];
